88a2c6ae9c4423dfc09448b37a15d8011230124e
Jinfeng Jiang <jeffjiang8@gmail.com>
2019-09-04 19:08:13 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -3,7 +3,7 @@ def map_to_negativize (array){
   neg_array = []
   i = 0 
   while i < array.length do
-    neg_array << -1 * array[i]
+    neg_array.push( -1 * array[i])
     i += 1
   end
   neg_array
